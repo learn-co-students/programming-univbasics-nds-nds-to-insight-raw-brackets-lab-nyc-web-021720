b3aec6c431012d8c66420967e96d05f30568a9fa
isabel k. lee <hello@isabelklee.co>
2020-02-06 01:33:11 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,22 +1,33 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
+require 'pry'
+
+# In this lab, you're going to work through the directors database and create a
+# `Hash` that records the director's name as a key, and the total grosses of all of their
+# movies as the value. For example:
+
+# ```ruby
+# hash = {
+#   "1st Director's Name"=>1234567890,
+#   "2nd Director's Name"=>1234577890,
+#   "3rd Director's Name"=>1234709136,
+#   ...
+# }
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+  results = {}
+  row_index = 0
+  
+  while row_index < nds.length do
+    director_name = nds[row_index][:name]
+    results[director_name] = 0
+    movie_index = 0
+      while movie_index < nds[row_index][:movies].length do 
+        results[director_name] += nds[row_index][:movies][movie_index][:worldwide_gross]
+        movie_index += 1
+      end 
+    row_index += 1
+  end
+  results
 end

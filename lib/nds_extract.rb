3c28d346e6dfc780519eff83c69db525e5d1012c
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-07 21:54:40 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,49 +4,38 @@ require "pry"
 
 def directors_totals(nds)
   result = {}
+
   name_index = 0
+    while name_index < nds.length do
 
-  while name_index < nds.length do
-    # binding.pry
-    director_name = nds[name_index][:name]
-    result [director_name]
-    binding.pry
-    nds[name_index]
-    name_index += 1
-  end
-    movie_index = 0 
+      director_name = nds[name_index][:name]
+      result[director_name] = 0 # accessing via key value pair. matched it with resuult and set variable in [] to equal 0 (for now)
+      movies_array = nds[name_index][:movies]
 
+      movie_index = 0
+      while  movie_index < movies_array.length do
+        result[director_name] += movies_array[movie_index][:worldwide_gross]
+        movie_index += 1
+      end
 
-    movie_index = 0
-    s_movies = nds[name_index][:movies]
-    while movie_index < s_movies.count {
-
-    }
+      name_index += 1
     end
 
-
-  # total += nds[name_index][:movies][movie_index][:worldwide_gross].sum
-  # movie_index = 0
-  s_movies = nds[0][:movies]
-  movie_index = 0
-
-  while movie_index < s_movies.count {
-    nds[0][:movies][movie_index][:worldwide_gross]
-  }
-  movie_index += 1
+    result
   end
-  total
 
 
-  # nds[0][:movies][0, movie_count][:worldwide_gross]
+    # movie_index = 0
+    # s_movies = nds[name_index][:movies]
+    # while movie_index < s_movies.count {
+    #
+    # }
+    # end
 
-  # binding.pry
 
-  # while name_index < nds.length {
-  #   << {name_index: }
-  # }
-  #
-  #
+
+
+
   # while nds[name_index][:movies]
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
@@ -63,10 +52,3 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-
-end
-# nds.length = 9
-
-# nds[0][:movies][0][:worldwide_gross] just 1 movie
-# loop the nds[name_index][:movies][movie_index][:worldwide_gross]
-# nds[0] => 1st director + all details
